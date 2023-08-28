88191cef98b22cbda86b59c804c39688fd695bcd
lvcky-gg <lvcky@Lvcky>
2023-08-28 12:45:35 -0500

    init

---
+++ index.py
@@ -0,0 +1,21 @@
+# Given an array of integers nums and an integer target,
+# return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1]
+
+
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if nums[i] + nums[j] == target and i != j:
+                return [i, j]
+    return None
+
+
+print(two_sum([2, 7, 11, 15], 9))
